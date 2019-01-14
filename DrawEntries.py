6c9b8567bd6ae3cdeb1779291497d2abcd8ae773
Syed Shameer Sarwar <syedshameersarwar@gmail.com>
2019-01-15 00:56:11 +0500

    Draw Solved Maze.

--- DrawEntries.py
+++ DrawEntries.py
@@ -1,38 +1,57 @@
 from mazelib import *
 import numpy as np
-from matplotlib.path import Path
-from matplotlib.patches import PathPatch
+import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 from copy import deepcopy
+
 
 def showPNG(maze):
     """Generate a simple image of the maze."""
     gridCopy = deepcopy(maze.grid)
+
     start_row , start_col = m.start
     end_row, end_col = m.end
-    gridCopy[start_row][start_col]=5
-    gridCopy[end_row][end_col]=10
+
+    gridCopy[start_row][start_col]=2.5 #random values to change colormap
+    gridCopy[end_row][end_col]=3.5
+
+    solution = maze.solutions[0]
+
+    if solution:
+
+        for r,c in solution:
+            gridCopy[r][c]=4.0
+
+        start_patch = mpatches.Patch(color='tab:blue', label='Start')
+        end_patch = mpatches.Patch(color='turquoise', label='End')
+               
+            
     
-    plt.figure(figsize=(10, 5), facecolor=("pink"), edgecolor=("red"))
-    plt.imshow(gridCopy, cmap=plt.cm.binary)
-    #plt.xticks([]), plt.yticks([])
+    plt.figure(figsize=(10, 5))
+
+    plt.legend(handles = [start_patch,end_patch], \
+               loc='center left', bbox_to_anchor=(1, 0.5))
+    
+    plt.imshow(gridCopy, cmap=plt.cm.ocean,interpolation = "nearest")
+    plt.xticks([]), plt.yticks([])
     plt.show()
 
+
+
+if __name__ == '__main__':
     
-m = Maze()
-m.generator = BacktrackingGenerator(25, 35)
-m.generate()
+    m = Maze()
 
-print(m)
+    m.generator = BacktrackingGenerator(25, 35)
+    m.generate()
+    m.generate_entrances()
 
-m.solver = WallFollower()
+    m.solver = WallFollower()
 
-m.generate_entrances()
+    m.solve()
 
-print(m.start,m.end)
+    showPNG(m)
 
-showPNG(m)
 
-m.solve()
 
-print(m)
+
