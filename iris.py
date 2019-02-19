5423e3b0a00017d0f6de224c2c83a6d296718611
kazzastic <rkazim79@gmail.com>
2019-02-19 17:29:17 +0500

    iris dataset example

---
+++ iris.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from sklearn.datasets import load_iris
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split 
+from sklearn import metrics
+from numpy import *
+
+
+x = iris.data
+y = iris.target
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=4)
+
+logreg = LogisticRegression()
+logreg.fit(x_train, y_train)
+
+y_pred = logreg.predict(x_test)
+print(metrics.accuracy_score(y_test, y_pred))
+'''
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(x_train, y_train)
+y_pred = knn.predict(x_test)
+print(metrics.accuracy_score(y_test, y_pred))
+
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(x_train, y_train)
+y_pred = knn.predict(x_test)
+print(metrics.accuracy_score(y_test, y_pred))
+'''
