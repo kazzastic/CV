00bc45610133900e0b4593c15d1bd8e6e0766065
kazzastic <rkazim79@gmail.com>
2019-02-19 05:01:42 +0500

    the model is trained and tested

--- housing.py
+++ housing.py
@@ -1,6 +1,9 @@
 import os
 import tarfile
 from six.moves import urllib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
@@ -14,11 +17,18 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     housing_tgz = tarfile.open(tgz_path)
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
-    
-import pandas as pd
 
 def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
 
+def make():
+    housing.hist(bins=50, figsize=(20,15))
+    plt.show()
 
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
