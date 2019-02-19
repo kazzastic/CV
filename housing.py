e978845106e1a5d7a279926e97cf0becbec2e880
kazzastic <rkazim79@gmail.com>
2019-02-19 22:01:16 +0500

    housing file update

--- housing.py
+++ housing.py
@@ -1,6 +1,7 @@
 import os
 import tarfile
 from six.moves import urllib
+from sklearn.model_selection import train_test_split 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -26,9 +27,20 @@ def make():
     housing.hist(bins=50, figsize=(20,15))
     plt.show()
 
+'''
+split_train_test fucntion decleration pretty much
+useless since sklearn already have a pre-defined 
+function!
+'''
 def split_train_test(data, test_ratio):
     shuffled_indices = np.random.permutation(len(data))
     test_set_size = int(len(data) * test_ratio)
     test_indices = shuffled_indices[:test_set_size]
     train_indices = shuffled_indices[test_set_size:]
     return data.iloc[train_indices], data.iloc[test_indices]
+
+fetch_housing_data()
+housing = load_housing_data()
+print housing.head()
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state = 42)
+#print train_set,test_set
