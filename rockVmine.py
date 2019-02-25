467a01ad3beb391986a53898b9992db387d1d4bc
kazzastic <rkazim79@gmail.com>
2019-02-26 04:56:22 +0500

    file

---
+++ rockVmine.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 25 17:37:35 2019
+
+@author: kazzastic
+"""
+
+import urllib2
+import sys
+
+target_url = ("https://archive.ics.uci.edu/ml/machine-learning-")
