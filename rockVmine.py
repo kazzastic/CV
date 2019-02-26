94e2a4d3e450e4ad94527bb6946460de913d9f70
kazzastic <rkazim79@gmail.com>
2019-02-26 06:09:59 +0500

    wine quality start

--- rockVmine.py
+++ rockVmine.py
@@ -1,12 +1,20 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 25 17:37:35 2019
+__author__ = "kazim"
+import os
+import pandas as pd
+#import statsmodels.formula.api as smf
+#import statsmodels.stats.multicomp as multi
+import scipy.stats
+import numpy as np
+#import seaborn
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
 
-@author: kazzastic
-"""
+PATH = os.path.join("datasets", "wine")
 
-import urllib2
-import sys
+def load_wine_data(wine_path=PATH):
+    csv_path = os.path.join(wine_path, "winequality-red.csv")
+    return pd.read_csv(csv_path)
 
-target_url = ("https://archive.ics.uci.edu/ml/machine-learning-")
+red = load_wine_data()
+#white = pd.read_csv('winequality-white.csv', low_memory=False, sep=';')
