b2444be49d64a51b501b6d43ccdfb7cbb5f53ec4
kazzastic <rkazim79@gmail.com>
2019-03-08 19:08:24 +0500

    rotation in openCV

--- imutils.py
+++ imutils.py
@@ -7,3 +7,14 @@ def translate(image , x, y):
     M = np.float32([[1, 0, x], [0, 1, y]])
     shifted = cv2.warpAffine(image ,M, (image.shape[1], image.shape[0]))
     return shifted 
+
+def rotate(image, angle, center = None, scale = 1.0):
+    h = image.shape[0]
+    w = image.shape[1]
+
+    if center is None:
+        center = (w//2, h//2)
+
+    M = cv2.getRotationMatrix2D(center, angle, scale)
+    rotated = cv2.warpAffine(image, M, (w,h))
+    return rotated
