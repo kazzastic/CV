b0757fa82814f409cd9d802a1587cc5d2a4ece16
kazzastic <rkazim79@gmail.com>
2019-03-09 15:10:17 +0500

    SCIT dataset

---
+++ scit.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  9 14:54:55 2019
+
+@author: kazzastic
+"""
+import numpy as np
+import os
+import tarfile
+import matplotlib.pyplot as plt
+import pandas as pd
+
+DATA_PATH = os.path.join("datasets", "housing")
+
+
+def load_housing_data(data_path = DATA_PATH):
+    csv_path = os.path.join(data_path, "SCIT.csv")
+    return pd.read_csv(csv_path)
