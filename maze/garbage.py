e853649b7bb9ec8702895aae94250020dbc5b1f8
kazzastic <rkazim79@gmail.com>
2019-05-18 03:40:41 +0500

    ...

--- maze/garbage.py
+++ maze/garbage.py
@@ -12,43 +12,6 @@ import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 from copy import deepcopy
 
-ile = 'mazegreen.jpg'
-img = cv2.imread(file)
-cv2.imshow('The Initial', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-    
-gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imshow('Gray', gray_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-    
-blurred = cv2.GaussianBlur(gray_img, (5,5), 0)
-cv2.imshow('Blur', blurred)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-    
-(t, thresh) = cv2.threshold(blurred, 155, 255, cv2.THRESH_BINARY)
-cv2.imshow('Threshold Binary', thresh)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-arr = np.array(thresh)
-
-for i in range(0, 300):
-    for j in range(0, 264):    
-        if(arr[i][j] == 255):        
-            arr[i][j] = 1
-
-
-for i in range(0, 300):
-    for j in range(0, 264):    
-        if(arr[i][j] == 1):        
-            arr[i][j] = 120
-
-cv2.imshow('Result', arr)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
 
 m = Maze()
@@ -59,6 +22,7 @@ m.solver = WallFollower()
 m.generate_entrances()
 m.solve()
 sol = m.solutions
+sol = np
 maze = m.grid
 '''
 def showPNG(maze):
