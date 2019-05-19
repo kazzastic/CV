0ef294be4c5ee40ff8ac743132e2c75f8560219f
kazzastic <rkazim79@gmail.com>
2019-05-19 09:10:14 +0500

    ...

--- maze/garbage.py
+++ maze/garbage.py
@@ -5,65 +5,41 @@ Created on Fri May 17 21:05:29 2019
 
 @author: kazzastic
 """
-
-from mazelib import *
+import cv2
 import numpy as np
-import matplotlib.patches as mpatches
-import matplotlib.pyplot as plt
-from copy import deepcopy
+import threading 
+#import colorsys
 
+class Point(object):
+    
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+    
+    def __add__(self, other):
+        return Point(self.x+other.x, self.y+other.y)
+    
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+    
+rw = 2
+p = 0 
+start = Point()
+end = Point()
 
+dir4 = [Point(0, 1), Point(0, -1), Point(1, 0), Point(-1, 0)]
 
-m = Maze()
-m.generator = Prims(27, 34)
-m.generate()
-
-m.solver = WallFollower()
-m.generate_entrances()
-m.solve()
-sol = m.solutions
-sol = np
-maze = m.grid
-'''
-def showPNG(maze):
-    """Generate a simple image of the maze."""
-    gridCopy = deepcopy(maze.grid)
-
-    start_row , start_col = m.start
-    end_row, end_col = m.end
+#def mouse_mov(event, pX, pY, flags, params):
+    #global 
     
     
-    gridCopy[start_row][start_col]=2.5 #random values to change colormap
-    gridCopy[end_row][end_col]=3.5
-
-    solution = maze.solutions[0]
-
-    if solution:
-
-        for r,c in solution:
-            gridCopy[r][c]=4.0
-
-        start_patch = mpatches.Patch(color='tab:blue', label='Start')
-        end_patch = mpatches.Patch(color='turquoise', label='End')
-
-
-
-    plt.figure(figsize=(10, 5))
-
-    plt.legend(handles = [start_patch,end_patch], \
-               loc='center left', bbox_to_anchor=(1, 0.5))
-
-    plt.imshow(gridCopy, cmap=plt.cm.ocean,interpolation = "nearest")
-    plt.xticks([]), plt.yticks([])
-    plt.show()
-
-if __name__ == '__main__':
     
-    m = Maze()
-    m.generator = BacktrackingGenerator(25, 35)
-    m.generate()
-    m.generate_entrances()    
-    m.solver = WallFollower()
-    showPNG(m)
-'''
     
+if __name__ == "__main__":
+    
+    img = cv2.imread('mazegreen.jpg', cv2.IMREAD_GRAYSCALE)
+    t, img = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    h, w= img.shape[:2]
+    
+        
