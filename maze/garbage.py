e090f79a7109f7be9217da4abdfb3619e2ffa122
kazzastic <rkazim79@gmail.com>
2019-05-19 09:58:03 +0500

    BFS

--- maze/garbage.py
+++ maze/garbage.py
@@ -29,10 +29,27 @@ end = Point()
 
 dir4 = [Point(0, 1), Point(0, -1), Point(1, 0), Point(-1, 0)]
 
-#def mouse_mov(event, pX, pY, flags, params):
-    #global 
+def mouse_mov(event, pX, pY, flags, param):
+    global img, start, end, p
     
-    
+    if event == cv2.EVENT_LBUTTONUP:
+        if p == 0:
+            cv2.rectangle(img, (pX - rw, pY - rw), (pX + rw, pY + rw), (0,0,255), -1)
+            start = Point(pX, pY)
+            print("start = ", start.x, start.y)
+            p +=1
+        elif p == 1:
+            cv2.rectangle(img, (pX - rw, pY - rw), (pX + rw, pY + rw), (0, 200, 50), -1)
+            end = Point(pX, pY)
+            print("end = ", end.x, end.y)
+            p+=1
+def display():
+    global img
+    cv2.imshow("Image", img)
+    cv2.setMouseCallback('Image', mouse_mov)
+    while True:
+        cv2.imshow("image", img)
+        cv2.waitKey(1)
     
     
 if __name__ == "__main__":
@@ -42,4 +59,10 @@ if __name__ == "__main__":
     img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
     h, w= img.shape[:2]
     
-        
+    print("Select start and end points : ")
+    t = threading.Thread(target=display, args=())
+    t.daemon = True
+    t.start()
+    while p<2:
+        pass
+    cv2.waitKey(0)
