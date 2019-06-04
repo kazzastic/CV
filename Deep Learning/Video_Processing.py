46522a4a384f2bffcf0d2851234535fa93f5e6a0
kazzastic <rkazim79@gmail.com>
2019-06-04 13:39:36 +0500

    video local save

--- Deep Learning/Video_Processing.py
+++ Deep Learning/Video_Processing.py
@@ -8,17 +8,50 @@ Created on Tue Jun  4 12:41:19 2019
 
 import cv2
 
-cap = cv2.VideoCapture(0)
-
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-while True:
-    ret,frame = cap.read()
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('This is me', frame)
+def show_video():
+    cap = cv2.VideoCapture(0)
     
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    
+    while True:
+        ret,frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('This is me', gray)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+def video_save():
+    cap = cv2.VideoCapture(0)
+    
+    #width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    #height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('Kazim.avi',fourcc, 20.0, (640,480))
+    
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret==True:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            
+            # write the flipped frame
+            out.write(frame)
+    
+            cv2.imshow('frame',frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            break
+    
+    # Release everything if job is finished
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+    
+video_save()
